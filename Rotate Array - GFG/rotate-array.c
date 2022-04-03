4f14f8d46d697c7de2de84757c88ac39503cb70a
aman0801 <77926256+aman0801@users.noreply.github.com>
2022-04-03 17:38:24 +0530

    Added solution - LeetHub

---
+++ Rotate Array - GFG/rotate-array.c
@@ -0,0 +1,53 @@
+// { Driver Code Starts
+#include<stdio.h>
+
+
+ // } Driver Code Ends
+//User function Template for C
+
+//Function to rotate an array by d elements in counter-clockwise direction. 
+void rotateArr(int arr[], int d, int n){
+    // code here
+    int temp[d];//=new int[d];
+    for(int i=0;i<d;i++){
+        temp[i]=arr[i];
+    }
+    for(int i=0;i<n-d;i++){
+        arr[i]=arr[i+d];
+    }
+    int count=0;
+    for(int i=(n-d);i<n;i++){
+        arr[i]=temp[count++];
+    }
+}
+
+// { Driver Code Starts.
+
+int main() {
+	int t;
+	//taking testcases
+	scanf("%d",&t);
+	
+	while(t--){
+	    int n, d;
+	    
+	    //input n and d
+	    scanf("%d%d",&n, &d);
+	    
+	    int arr[n];
+	    
+	    //inserting elements in the array
+	    for(int i = 0; i < n; i++){
+	        scanf("%d",&arr[i]);
+	    }
+	    //calling rotateArr() function
+	    rotateArr(arr, d,n);
+	    
+	    //printing the elements of the array
+	    for(int i =0;i<n;i++){
+	        printf("%d ",arr[i]);
+	    }
+	    printf("\n");
+	}
+	return 0;
+}  // } Driver Code Ends

9ba03589feee5ec833772c453b88bf9c5ee2162b
aman0801 <77926256+aman0801@users.noreply.github.com>
2022-04-06 11:25:48 +0530

    Added solution - LeetHub

---
+++ Check if array is sorted - GFG/check-if-array-is-sorted.c
@@ -0,0 +1,41 @@
+// { Driver Code Starts
+//Initial Template for C
+
+#include<stdio.h>
+#include<stdbool.h>
+
+
+ // } Driver Code Ends
+//User function Template for C
+
+bool arraySortedOrNot(int arr[], int n) {
+
+    //code here
+     for(int i =0;i<n-1;i++){
+       if(arr[i]>arr[i+1]){
+           return false;
+       }
+   }
+   return true;
+    
+    
+}
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    while (t--) {
+        int n;
+        scanf("%d", &n);
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &arr[i]);
+        }
+        bool ans = arraySortedOrNot(arr, n);
+        printf("%d\n", ans);
+
+    }
+    return 0;
+}  // } Driver Code Ends

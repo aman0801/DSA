b4654506241b223481b860db3ac8598619e62c72
aman0801 <77926256+aman0801@users.noreply.github.com>
2022-07-30 23:28:09 +0530

    Time: 12 ms (35.39%), Space: 10.1 MB (59.60%) - LeetHub

---
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        vector<int>count;
+        int i=0,j=0;
+        while(i<nums1.size()&&j<nums2.size()){
+                if(nums1[i]==nums2[j]){
+                    count.push_back(nums1[i]);
+                i++;
+                    j++;
+                }
+                else if(nums1[i]<nums2[j]){
+            i++;
+        }
+        else if(nums2[j]<nums1[i]){
+            j++;
+        }
+            }
+        
+        return count;
+    }
+};

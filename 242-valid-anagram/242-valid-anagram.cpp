513772a21cbdc8ff5482769a942115f262d12351
aman0801 <77926256+aman0801@users.noreply.github.com>
2022-08-02 21:09:36 +0530

    Time: 24 ms (19.74%), Space: 7.1 MB (94.25%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -2,10 +2,13 @@ class Solution {
 public:
     bool isAnagram(string s, string t) 
     {
+        //Method -1 --sorting
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
         
         return s==t;
+        
+        
         
     }
 };

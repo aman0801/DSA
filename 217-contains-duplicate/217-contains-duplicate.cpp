705388cc320b27d24c93bd20d6cf31f4a755e5b5
aman0801 <77926256+aman0801@users.noreply.github.com>
2022-08-06 14:27:47 +0530

    Time: 158 ms (44.04%), Space: 46.6 MB (91.89%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
+//             Brute force method
         sort(nums.begin(),nums.end());
         
-        for(int i=1;i<nums.size();i++){
+        for(int i=1;i<size(nums);i++){
             if(nums[i-1]==nums[i])
                 return true;
-            // }else{
-            //     return false;
-            // }
-        }
+             }//else{
+        //         return false;
+        //     }
+        // }
             return false;
-            
+    
     }
+           
+//         unordered_map<int,int>mp;
+//         for(int x:nums){
+//             mp[x]++;
+//         }
+//         for(int x:nums){
+//             if(mp[x]>=2){
+//                 return true;
+//             }
+//         }
+//         return false;
+        
+//     }
 };

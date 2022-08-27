939916a58abe62dc84f5b880f5bcc4e483d8a661
Aman Singh <77926256+aman0801@users.noreply.github.com>
2022-08-27 22:21:37 +0530

    Time: 112 ms (17.73%), Space: 21 MB (6.08%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -9,41 +9,53 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode* temp1=headA;
-        ListNode* temp2=headB;
-        int count=0;
-        int count1=0;
-        while(temp1!=NULL){
-            temp1=temp1->next;
-            count++;
-        }
-        while(temp2!=NULL){
-            temp2=temp2->next;
-            count1++;
-        }
+//         ListNode* temp1=headA;
+//         ListNode* temp2=headB;
+//         int count=0;
+//         int count1=0;
+//         while(temp1!=NULL){
+//             temp1=temp1->next;
+//             count++;
+//         }
+//         while(temp2!=NULL){
+//             temp2=temp2->next;
+//             count1++;
+//         }
         
-        int diff=abs(count-count1);
+//         int diff=abs(count-count1);
         
-        if(count>count1){
-            while(diff){
-                headA=headA->next;
-                diff--;
-            }
-        }else{
-            while(diff){
-                headB=headB->next;
-                diff--;
-            }
-        }
+//         if(count>count1){
+//             while(diff){
+//                 headA=headA->next;
+//                 diff--;
+//             }
+//         }else{
+//             while(diff){
+//                 headB=headB->next;
+//                 diff--;
+//             }
+//         }
         
-        while(headA!=NULL and headB!=NULL){
-            if(headA==headB){
-                return headA;
-            }else{
-                headA=headA->next;
-                headB=headB->next;
-            }
-        }
-        return NULL;
+//         while(headA!=NULL and headB!=NULL){
+//             if(headA==headB){
+//                 return headA;
+//             }else{
+//                 headA=headA->next;
+//                 headB=headB->next;
+//             }
+//         }
+//         return NULL;
+        unordered_map<ListNode*, int> m;
+		while(headA != NULL){
+			m[headA]++;
+			headA = headA -> next;
+		}
+		while(headB != NULL){
+			if(m[headB] > 0){
+				return headB;
+			}
+			headB = headB -> next;
+		}
+		return NULL;
     }
 };

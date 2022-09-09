c89c5154d8df11e8f50526670c58ef9a4efd20b9
Aman Singh <77926256+aman0801@users.noreply.github.com>
2022-09-10 00:00:03 +0530

    Time: 4 ms (44.59%), Space: 6.3 MB (54.25%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.cpp
+++ 771-jewels-and-stones/771-jewels-and-stones.cpp
@@ -11,9 +11,10 @@ public:
         for(int i=0;i<s.size();i++){
             if(mp.find(s[i])!=mp.end()){
                 mp[s[i]]+=1;
+                c++;
             }
         }
-        for(int i=0;i<j.size();i++) c+=mp[j[i]];
+        // for(int i=0;i<j.size();i++) c+=mp[j[i]];
         return c;
     }
 };

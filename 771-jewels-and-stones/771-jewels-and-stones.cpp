bac5e9a642d66cb76c27f688e2b79c8984da2eac
Aman Singh <77926256+aman0801@users.noreply.github.com>
2022-09-09 23:59:14 +0530

    Time: 3 ms (63.05%), Space: 6.4 MB (43.98%) - LeetHub

---
+++ 771-jewels-and-stones/771-jewels-and-stones.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numJewelsInStones(string j, string s) {
+        unordered_map<char,int>mp;
+        
+        int c =0;
+        for(auto i:j){
+            mp[i++];
+        }
+        
+        for(int i=0;i<s.size();i++){
+            if(mp.find(s[i])!=mp.end()){
+                mp[s[i]]+=1;
+            }
+        }
+        for(int i=0;i<j.size();i++) c+=mp[j[i]];
+        return c;
+    }
+};

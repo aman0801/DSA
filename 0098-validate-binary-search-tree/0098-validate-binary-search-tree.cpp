d8c6b0a09a3e177d13aebb5583e4c33ffb11cc59
Aman Singh <amansingh20010801@gmail.com>
2023-01-10 00:05:57 +0530

    Time: 7 ms (98.04%), Space: 21.6 MB (93.22%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -11,20 +11,17 @@
  */
 class Solution {
 public:
-    vector<int>ans;
-    void find(TreeNode* root){
-        if(!root)return;
-        find(root->left);
-        ans.push_back(root->val);
-        find(root->right);
+    bool find(TreeNode* root, long min, long max){
+        if(root == NULL)return true;
+        
+        if(root->val>min && root->val<max){
+           return find(root->left, min, root->val) &&
+            find(root->right, root->val, max);
+        }
+            return false;
+        
     }
     bool isValidBST(TreeNode* root) {
-        find(root);
-         for (int i=0; i<ans.size()-1; i++) {
-            if (ans[i] >= ans[i+1]) {
-                return false;
-            }
-        }
-        return true;
+        return find(root, LONG_MIN, LONG_MAX);
     }
 };

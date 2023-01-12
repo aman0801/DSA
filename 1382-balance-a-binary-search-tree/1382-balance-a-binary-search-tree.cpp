c5bf9ebe7b347eeabb0e5877bc5319cd285f79d0
Aman Singh <amansingh20010801@gmail.com>
2023-01-12 15:03:32 +0530

    Time: 212 ms (54.81%), Space: 63.4 MB (27.08%) - LeetHub

--- 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
@@ -9,31 +9,31 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-// vector<int>ans;
-
 class Solution {
 public:
-   void helper(TreeNode* root, vector<int>&ans){
-     if(!root) 
-            return; 
-    helper(root->left, ans);
-    ans.push_back(root->val);
-    helper(root->right, ans);
-}
-    TreeNode* insert(vector<int>&ans,int s, int e){
+    void find(TreeNode* root, vector<int> &arr)
+    {
+        if(!root) return;
+        find(root->left,arr);
+        arr.push_back(root->val);
+        find(root->right,arr);        
+    }
+    TreeNode* ans(int s, int e, vector<int>&arr){
         if(s>e)return NULL;
-        int m = (s+e)/2;
-        // TreeNode* node = ans[m];
-        TreeNode* node = new TreeNode(ans[m]);
-
-        node->left = insert(ans,s, m - 1);
-        node->right = insert(ans,m + 1, e);
+        int mid = (s+e)/2;
+        
+        TreeNode* node = new TreeNode(arr[mid]);
+        node->left = ans(s, mid-1, arr);
+        node->right = ans(mid+1, e, arr);
         return node;
     }
     TreeNode* balanceBST(TreeNode* root) {
-        vector<int>ans;
-        helper(root, ans);
-        return insert(ans,0, ans.size()-1);
+        vector<int>arr;
+        find(root, arr);
+        for(int i=0;i<arr.size();i++){
+            cout<<arr[i];
+        }
+        return ans(0, arr.size()-1, arr);
+        
     }
 };
-

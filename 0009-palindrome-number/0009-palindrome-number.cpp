63c3c70e61856021aed4e5e54284e1c90a4a9773
Aman Singh <amansingh20010801@gmail.com>
2023-01-14 23:18:33 +0530

    Time: 20 ms (58.59%), Space: 5.9 MB (51.18%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        string s = to_string(x); 
-        string s2 = s; 
-        reverse(s2.begin(), s2.end()); 
-        
-        if(s == s2) {
-	    return true;
-       } else {
-	   return false;
-         }
-
+        if(x<0|| (x!=0 &&x%10==0)) return false;
+        int sum=0;
+        while(x>sum)
+        {
+            sum = sum*10+x%10;
+            x = x/10;
+        }
+        return (x==sum)||(x==sum/10);
     }
 };

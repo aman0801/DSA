10e765d10c86f6e09e1fb1d84f59bc1acf5d2c01
Aman Singh <amansingh20010801@gmail.com>
2023-02-09 23:30:13 +0530

    Time: 959 ms (42.28%), Space: 93.4 MB (13.82%) - LeetHub

---
+++ 2306-naming-a-company/2306-naming-a-company.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    long long distinctNames(vector<string>& ideas) {
+        unordered_map<string, int>m;
+        for (auto i : ideas) 
+            m[i]++;
+
+        vector<vector<int>>dp(26, vector<int>(26, 0));
+
+        for (int i = 0; i < ideas.size(); i++) {
+            string curr = ideas[i];
+            char ch1 = curr[0];
+            for (int j = 0; j < 26; j++) {
+                char ch2 = j+'a';
+                curr[0] = ch2;
+                if (m.find(curr) == m.end()) {
+                    dp[ch1-'a'][j]++;
+                }
+            }
+        }
+
+        long long ans = 0;
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < 26; j++) {
+                ans += dp[i][j] * dp[j][i];
+            }
+        }
+        return ans;
+    }
+};

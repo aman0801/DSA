84ac51bba80c56e8f71d6a39d0d872a25e0e5045
Aman Singh <amansingh20010801@gmail.com>
2023-02-21 01:24:28 +0530

    Time: 32 ms (20.63%), Space: 17.9 MB (11.02%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
-    
-    void DFS(vector<vector<int>>& graph, vector<vector<int>>&ans, vector<int>&path, int curr){
-        path.push_back(curr);
-        
-        if(curr == graph.size()-1){
-            ans.push_back(path);
-        }else{
-            for(auto &x: graph[curr]){
-                DFS(graph, ans, path, x);
-            }
-        }
-        path.pop_back();
-    }
-    
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
         vector<vector<int>>ans;
         vector<int>path;
-        DFS(graph, ans, path, 0);
+        int s = 0;
+        path.push_back(s);
+        
+        queue<vector<int> >q;
+        q.push(path);
+        
+        while(!q.empty()){
+            path=q.front();
+            q.pop();
+            
+            int val=path.back();
+            
+            if(val == graph.size()-1){
+                ans.push_back(path);
+            }
+            
+            for(int &i:graph[val]){
+                vector<int>v(path);
+                v.push_back(i);
+                q.push(v);
+            }
+            
+        }
         return ans;
     }
 };

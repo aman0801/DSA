ea76913f838bf42fb64217a8676ff56911d8ed6c
Aman Singh <amansingh20010801@gmail.com>
2023-03-27 23:15:00 +0530

    Time: 41 ms (22.24%), Space: 50.9 MB (7.07%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
-    string convert(string s, int r) {
-        int n=s.size();
-        if(r==1){
-            return s;
-        }
-        vector<vector<char>>arr(r,vector(n,'0'));
-        int limit=r-1;
-        int i=0,j=0;
-        int k=0;
-        int key1=1;
-        int key2=0;
-        int count=0;
-        while(k<n){
-            if(i==-1){key1=1;i=1;}
-            if(i==r){key1=0;i=r-2;}
-            cout<<i<<" "<<j<<" "<<s[k]<<endl;
-            arr[i][j]=s[k];
-            if(key2){j++;}
-            if(key1){i++;}else{i--;}
-            if(i==r){key2=1;j++;}
-            if(i==-1){key2=0;}
-            k++;
-        }
-        string ans="";
-        for(int i=0;i<r;i++){
-            for(int j=0;j<n;j++){
-                if(arr[i][j]!='0'){
-                    ans+=arr[i][j];
+    string convert(string s, int numRows) {
+        if(numRows == 1)return s;
+        int i = 0;
+        int row = 0;
+        vector<string>zig(numRows);
+        bool direction = 1;
+        
+        while(true){
+            if(direction){
+                while(row<numRows && i<s.size()){
+                    zig[row++].push_back(s[i++]);
                 }
+                row = numRows-2;
+            }else{
+                while(row>=0 && i<s.size()){
+                    zig[row--].push_back(s[i++]);
+                }
+                row = 1;
             }
+            if(i>=s.size()){
+                break;
+            }
+            direction=!direction;
+        }
+        string ans = "";
+        for(int i=0; i<zig.size(); i++){
+            ans = ans + zig[i];
         }
         return ans;
     }

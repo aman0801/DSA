1579475094531661c061721db59fb636be29ac63
Aman Singh <amansingh20010801@gmail.com>
2023-04-07 23:25:06 +0530

    Time: 130 ms (80.16%), Space: 57.1 MB (99.69%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,26 +1,14 @@
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& nums) {
-//             Brute force method
-//         sort(nums.begin(),nums.end());
-        
-//         for(int i=1;i<size(nums);i++){
-//             if(nums[i-1]==nums[i])
-//                 return true;
-//              }
-//             return false;
-    
-//     }
-           
-        unordered_map<int,int>mp;
-        for(int x:nums){
-            mp[x]++;
-        }
-        for(int x:nums){
-            if(mp[x]>=2){
+     bool containsDuplicate(vector<int>& nums) {
+         sort(nums.begin(), nums.end());
+       
+        for(int idx = 0; idx < nums.size() - 1; idx++) {
+            
+            if(nums[idx] == nums[idx + 1])
                 return true;
-            }
         }
+   
         return false;
         
     }

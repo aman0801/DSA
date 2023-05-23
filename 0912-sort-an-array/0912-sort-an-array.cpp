b6b082a7c951e30bab82332a3c1269de52f67f18
Aman Singh <amansingh20010801@gmail.com>
2023-05-24 01:21:03 +0530

    Time: 521 ms (37.55%), Space: 66.7 MB (82.18%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,7 +1,40 @@
 class Solution {
 public:
+    
+    void solve(vector<int>& nums, int l, int mid, int h){
+        int total = h - l + 1;
+        int gap = total%2 + total/2;
+        
+        while(gap > 0){
+            int i = l;
+            int j = l+gap;
+            while(j <= h){
+                if(nums[i] > nums[j]){
+                    swap(nums[i], nums[j]);
+                }
+                i++;
+                j++;
+            }
+             if (gap <= 1) {
+                gap = 0;
+            } else {
+                gap = (gap / 2) + (gap % 2); 
+            }
+        }
+        
+    }
+    
+    void mergesort(vector<int>&nums, int l, int h){
+        if(l>=h) return;
+
+        int mid = l + (h - l)/2;
+        mergesort(nums,l,mid);
+        mergesort(nums,mid+1,h);
+        solve(nums,l,mid,h);
+    }
+    
     vector<int> sortArray(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        mergesort(nums, 0, nums.size()-1);
         return nums;
     }
 };

e8d9e2e552e3ab23d1918fe6c38589d5e370d2fc
Aman Singh <amansingh20010801@gmail.com>
2023-05-26 08:48:16 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (97.09%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+//         int i=0;
+//         int j=piles.size()-1;
+//         int sum1 = 0;
+//         int sum2 = 0;
+//         while(i<j){
+//             if(piles[i]<piles[j]){
+//                 sum1 = sum1+piles[j];
+//                 j--;
+//             }else{
+//                 sum1 = sum1+piles[i];
+//                 i++;
+//             }
+            
+//             if(piles[i]<piles[j]){
+//                 sum2 = sum2+piles[j];
+//                 j--;
+//             }else{
+//                 sum2 = sum2+piles[i];
+//                 i++;
+//             }
+//         }
+//         if(sum>sum1){
+//             return true;
+//         }
+//         return false;
+        return true;
+    }
+};

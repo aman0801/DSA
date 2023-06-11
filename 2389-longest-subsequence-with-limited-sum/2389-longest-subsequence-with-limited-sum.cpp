a51b21b00088340de3411dfe35b86b93d6b309bb
Aman Singh <amansingh20010801@gmail.com>
2023-06-11 11:53:16 +0530

    Time: 14 ms (93.81%), Space: 13.8 MB (32.59%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
-    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
-         int n = nums.size();
-        int m = queries.size();
-        vector<int>ans(m,0);
-        
-        sort(nums.begin(), nums.end());
-        vector<int>pre;
-        int sum = 0;
-        
-        for(int i=0;i<nums.size();i++){
-            sum = sum + nums[i];
-            pre.push_back(sum);
-        }
-        
-        for(int i=0;i<queries.size();i++){
-            for(int j=0;j<nums.size();j++){
-                if(pre[j]<=queries[i]){
-                    ans[i] = j+1;
-                }else{
-                    break;
-                }
+    
+    int solve(vector<int>& nums, int target){
+        int l = 0;
+        int r = nums.size()-1;
+        int count = -1;
+        int m;
+        while(l<=r){
+            m = l + (r-l)/2;
+            if(nums[m] <= target){
+                count = m;
+                l = m+1;
+            }else{
+                r = m - 1;
             }
+        }
+        return count+1;
+    }
+    
+    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+        vector<int>ans;
+        sort(nums.begin(), nums.end());
+        for(int i=1; i<nums.size(); i++){
+            nums[i] = nums[i] + nums[i-1];
+        }
+        for(auto &i: queries){
+            int count = solve(nums, i);
+            ans.push_back(count);
         }
         return ans;
     }

9a124b3d5aeb6846519478d334cfa47d98f66049
Aman Singh <amansingh20010801@gmail.com>
2023-06-14 08:35:46 +0530

    Time: 4 ms (40.28%), Space: 6.2 MB (97.92%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -24,24 +24,3 @@ public:
         return st.empty();
     }
 };
-
-// stack<char> sack;
-//      int n = s.size();
-//      for(int i=0;i<n;i++)
-//      {   
-//          if(s[i]=='(' || s[i]=='{' || s[i]=='[')
-//          { 
-//            sack.push(s[i]);
-//            continue;
-//          }
-//          if(sack.empty())
-//              return false;
-//          char x = sack.top();
-//          if((x=='(' && s[i]==')') || (x=='{' && s[i]=='}') || (x=='[' && s[i]==']'))
-//              sack.pop();
-//          else
-//             return false;
-//      } 
-//      if(sack.empty())
-//          return true;
-//      return false;

eab479b680fb12fe2ad7995bb027744acaa5cfe8
Aman Singh <amansingh20010801@gmail.com>
2023-06-19 00:32:22 +0530

    Time: 234 ms (66.89%), Space: 84.1 MB (52.47%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.cpp
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -1,18 +1,21 @@
 class StockSpanner {
 public:
+    
     stack<pair<int, int>>st;
+    
     StockSpanner() {
         
     }
     
     int next(int price) {
-        int span = 1;
-        while(!st.empty() and st.top().first<=price){
-            span = span + st.top().second;
+        int ans = 1;
+        while(!st.empty() && st.top().first<=price){
+            ans = ans + st.top().second;
             st.pop();
         }
-        st.push({price, span});
-        return span;
+        st.push({price, ans});
+        
+        return st.top().second;
     }
 };
 

4eec7d26d349e123e617e37b949b6d32d1769639
Aman Singh <amansingh20010801@gmail.com>
2023-08-01 11:38:35 +0530

    Time: 880 ms (5.00%), Space: 524.2 MB (5.40%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void solve(int n, int k, unordered_set<int>& out, vector<vector<int>>& ans, int countk, int countn) {
+    void solve(int n, int k, unordered_set<int>& out, vector<vector<int>>& ans, int countn) {
         if (out.size() == k) {
             ans.push_back(vector<int>(out.begin(), out.end()));
             return;
@@ -11,15 +11,15 @@ public:
         }
 
         out.insert(countn);
-        solve(n, k, out, ans, countk, countn + 1);
+        solve(n, k, out, ans, countn + 1);
         out.erase(countn);
-        solve(n, k, out, ans, countk, countn + 1);
+        solve(n, k, out, ans, countn + 1);
     }
 
     vector<vector<int>> combine(int n, int k) {
         unordered_set<int> out;
         vector<vector<int>> ans;
-        solve(n, k, out, ans, 1, 1);
+        solve(n, k, out, ans, 1);
         return ans;
     }
 };

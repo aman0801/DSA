c709d496bb4531b25351253582929e9d326918f2
Aman Singh <amansingh20010801@gmail.com>
2023-08-03 18:06:47 +0530

    Added solution - LeetHub

--- Merge two binary Max heaps - GFG/merge-two-binary-max-heaps.cpp
+++ Merge two binary Max heaps - GFG/merge-two-binary-max-heaps.cpp
@@ -12,20 +12,39 @@ using namespace std;
 
 class Solution{
     public:
+    
+    void heapify(vector<int>&ans, int size, int i){
+        int largest = i;
+        int right = 2*i+2;
+        int left = 2*i+1;
+        
+        if(left<size && ans[left]>ans[largest]){
+            largest = left;
+        }
+        if(right<size && ans[right]>ans[largest]){
+            largest = right;
+        }
+        
+        if(largest!=i){
+           swap(ans[i],ans[largest]);
+           heapify(ans, size, largest);
+       } 
+        
+    }
+    
     vector<int> mergeHeaps(vector<int> &a, vector<int> &b, int n, int m) {
         // your code here
-        priority_queue<int>pq;
         vector<int>ans;
         for(auto &i:a){
-            pq.push(i);
+            ans.push_back(i);
         }
         for(auto &i:b){
-            pq.push(i);
+            ans.push_back(i);
         }
-        while(!pq.empty()){
-            int temp = pq.top();
-            pq.pop();
-            ans.push_back(temp);
+        
+        int temp = m+n;
+        for(int i=temp/2; i>=0; i--){
+            heapify(ans, temp, i);
         }
         return ans;
     }

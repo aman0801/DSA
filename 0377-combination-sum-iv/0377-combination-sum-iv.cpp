2b18000b9aa302a589c9ac6d82be2a273e56ede0
Aman Singh <amansingh20010801@gmail.com>
2023-09-09 19:26:49 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (8.61%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -2,27 +2,31 @@ class Solution {
 public:
     int n;
     int t[1001][201];
-    int solve(vector<int>& nums, int target, int idx) {
+    
+    int solve(int idx, vector<int>& nums, int target) {
         if(target == 0)
             return 1;
         
         if(idx >= n || target < 0)
             return 0;
         
+        int result = 0;
+        
         if(t[target][idx] != -1)
             return t[target][idx];
         
-        int take_idx    = solve(nums, target - nums[idx], 0);
-        int reject_idx  = solve(nums, target, idx+1);
+        for(int i = idx; i<n; i++) {
+            int take_i   = solve(0, nums, target-nums[i]);
+            
+            result += take_i;
+        }
         
-        return t[target][idx] = take_idx + reject_idx;
-        
+        return t[target][idx] = result;
     }
     
     int combinationSum4(vector<int>& nums, int target) {
         n = nums.size();
         memset(t, -1, sizeof(t));
-        
-        return solve(nums, target, 0);
+        return solve(0, nums, target);
     }
 };

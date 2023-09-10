81b03407749810a128889d3f4ac4e0e3974baa9b
Aman Singh <amansingh20010801@gmail.com>
2023-09-10 19:46:32 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (10.66%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int compareVersion(string v1, string v2) {
+        
+        int i = 0, j = 0, v1Len = v1.size(), v2Len = v2.size(), n1, n2;
+        while (i < v1.size() || j < v2.size()) {            
+            n1 = 0;
+            n2 = 0;
+            while (i < v1.size() && v1[i] != '.') n1 = n1 * 10 + (v1[i++] - '0');
+            while (j < v2.size() && v2[j] != '.') n2 = n2 * 10 + (v2[j++] - '0');
+            cout << n1 << n2 << endl;
+            if (n1 < n2) return -1;
+            else if (n1 > n2) return 1;
+            i++;
+            j++;
+        }
+        return 0;
+    }
+};

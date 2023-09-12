19486f01858b7758fa18b706e619dc83a8e4929c
Aman Singh <amansingh20010801@gmail.com>
2023-09-12 11:14:49 +0530

    Time: 47 ms (85.12%), Space: 17.4 MB (58.00%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -2,19 +2,28 @@ class Solution {
 public:
     int minDeletions(string s) {
         int freq[26] = {0};
-        unordered_set<int>st;
-        for(auto& i:s){
-            freq[i-'a']++;
-        }
         
-        int count = 0;
-        for(int i=0; i<26; i++){
-            while(freq[i]>0 && st.find(freq[i])!=st.end()){
-                freq[i] = freq[i]-1;
-                count++;
-            }
-            st.insert(freq[i]);
+        for(char &ch : s) {
+            freq[ch-'a']++;
         }
-        return count;
+        sort(begin(freq), end(freq));
+        
+        int result = 0;
+        
+        for(int i = 24; i >= 0 && freq[i] > 0; i--) {
+            
+            if(freq[i] >= freq[i+1]) {
+                
+                int prev = freq[i];
+                
+                freq[i] = max(0, freq[i+1]-1);
+                
+                result  += (prev - freq[i]);
+                
+            }
+            
+        }
+          
+        return result;
     }
 };

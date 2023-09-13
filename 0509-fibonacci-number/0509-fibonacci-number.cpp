fe5be111b7ec9b1a408d38a9a9c2416499ec4c37
Aman Singh <amansingh20010801@gmail.com>
2023-09-13 11:58:59 +0530

    Time: 2 ms (75.35%), Space: 6.1 MB (28.09%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -11,8 +11,23 @@ public:
         
     }
     
+    int solve(int n, vector<int>& dp){
+        if(n==0 || n==1){
+            return n;
+        }
+        
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        
+        dp[n] = solve(n-1, dp)+solve(n-2, dp);
+        return dp[n];
+    }
+    
     int fib(int n) {
-        int ans = solve(n);
+        // int ans = solve(n);
+        vector<int>dp(n+1, -1);
+        int ans = solve(n, dp);
         return ans;
     }
 };

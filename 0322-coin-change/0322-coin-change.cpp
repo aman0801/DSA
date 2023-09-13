676c1ded359f9c450b41cbabc7857a1e0d912210
Aman Singh <amansingh20010801@gmail.com>
2023-09-13 13:24:08 +0530

    Time: 60 ms (82.76%), Space: 14.6 MB (58.67%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -46,10 +46,33 @@ public:
         return dp[amount];
     }
     
+    int sol(vector<int>& coins, int amount){
+        
+        vector<int>dp(amount+1, INT_MAX);
+        
+        dp[0] = 0;
+        
+        for(int i=1; i<=amount; i++){
+            int mini = INT_MAX;
+            
+            for(int j=0; j<coins.size(); j++){
+                if(i-coins[j]>=0){
+                    int ans = dp[i-coins[j]];
+                    if(ans != INT_MAX){
+                        mini = min(ans+1, mini);
+                    }
+                }
+            }
+            dp[i] = mini;
+        }
+        return dp[amount];
+    }
+    
     int coinChange(vector<int>& coins, int amount) {
        // int ans = solve(coins, amount);
-        vector<int>dp(amount+1, -1);
-        int ans = solve(coins, amount, dp);
+        // vector<int>dp(amount+1, -1);
+        // int ans = solve(coins, amount, dp);
+        int ans = sol(coins, amount);
         if(ans == INT_MAX){
             return -1;
         }

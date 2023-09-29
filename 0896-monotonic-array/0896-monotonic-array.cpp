d5b5cae52cc67180d3f9118cdaf9806eb4d94b73
Aman Singh <amansingh20010801@gmail.com>
2023-09-29 10:58:52 +0530

    Time: 109 ms (93.24%), Space: 96.9 MB (13.85%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.cpp
+++ 0896-monotonic-array/0896-monotonic-array.cpp
@@ -7,10 +7,14 @@ public:
         for(int i = 1; i < nums.size(); i++) {
             if(nums[i - 1] > nums[i]) {
                 inc = false;  
+                break;
             }
-            
+        }
+        
+        for(int i = 1; i < nums.size(); i++) {
             if(nums[i - 1] < nums[i]) {
                 dec = false; 
+                break;
             }
         }
         

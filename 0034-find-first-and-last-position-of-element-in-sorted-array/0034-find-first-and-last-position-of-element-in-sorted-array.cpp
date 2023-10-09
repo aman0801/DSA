0289ddfa47d45beb04a81a217a02ac4ac309ce43
Aman Singh <amansingh20010801@gmail.com>
2023-10-09 10:01:57 +0530

    Time: 11 ms (9.25%), Space: 13.8 MB (37.79%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
     
-    int first(vector<int>& nums, int target){
+    int firstP(vector<int>& nums, int target){
         int i=0;
         int j=nums.size()-1;
         int ans = -1;
         while(i<=j){
             int mid = (i+j)/2;
-            
             if(nums[mid] == target){
                 ans = mid;
                 j = mid-1;
-            }else if(nums[mid]>target){
-                j = mid-1;
-            }else{
+            }else if(nums[mid]<target){
                 i = mid+1;
+            }else{
+                j = mid-1;
             }
         }
         return ans;
     }
     
-    int second(vector<int>& nums, int target){
+    int lastP(vector<int>& nums, int target){
         int i=0;
         int j=nums.size()-1;
         int ans = -1;
         while(i<=j){
             int mid = (i+j)/2;
-            
             if(nums[mid] == target){
                 ans = mid;
                 i = mid+1;
@@ -40,11 +38,8 @@ public:
     }
     
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int>ans(2, -1);
-        ans[0] = first(nums, target);
-        ans[1] = second(nums, target);
-        
-        return ans;
-        
+        int first = firstP(nums, target);
+        int last = lastP(nums, target);
+        return {first, last};
     }
 };

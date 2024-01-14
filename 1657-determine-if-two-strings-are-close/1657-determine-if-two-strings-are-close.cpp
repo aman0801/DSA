2f8ff013e5063477e13fdcf4fda6e361c77357f8
Aman Singh <amansingh20010801@gmail.com>
2024-01-14 14:07:21 +0530

    Time: 71 ms (83.43%), Space: 20.9 MB (89.19%) - LeetHub

---
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool closeStrings(string word1, string word2) {
+           if(word1.length()!=word2.length()){
+                   return false;
+           }
+            
+            vector<int>freq1(26, 0);
+            vector<int>freq2(26, 0);
+            
+            for(int i=0; i<word1.length(); i++){
+                    freq1[word1[i]-'a']++;
+                    freq2[word2[i]-'a']++;
+            }
+            
+            for(int i=0; i<26; i++){
+                    if(freq1[i]!=0 && freq2[i]!=0){
+                            continue;
+                    }
+                    if(freq1[i]==0 && freq2[i]==0){
+                            continue;
+                    }else{
+                            return false;
+                    }
+            }
+            sort(freq1.begin(), freq1.end());
+            sort(freq2.begin(), freq2.end());
+            return freq1==freq2;
+    }
+};

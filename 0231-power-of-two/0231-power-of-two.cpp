8bac5ddf7f9d32b0ac6d99331ad18ca22ff8f747
Aman Singh <amansingh20010801@gmail.com>
2024-02-19 09:28:08 +0530

    Time: 6 ms (20.09%), Space: 7.2 MB (19.05%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
-    bool isPowerOfTwo(int n) {
-            if(n==0){
-                    return 0;
-            }
-            if(n==1){
-                    return 1;
-            }
-            if(n%2!=0){
-                    return 0;
-            }
-            while(true){
-                    n=n/2;
-                    if(n==1){
-                            return 1;
-                    }
-                    if(n%2!=0){
-                            return 0;
-                    }
-            }
-            return 0;
-    }
+	bool isPowerOfTwo(int n) {
+		if(n <= 0){
+			return false;
+		}
+		if(n == 1){
+			return true;
+		}
+		if(n % 2 != 0){
+			return false;
+		}
+		return isPowerOfTwo(n / 2);
+	}
 };

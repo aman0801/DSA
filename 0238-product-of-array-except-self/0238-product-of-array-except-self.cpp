04e2020aa5352b0f9b6464c3547b1ba871bde07e
Aman Singh <amansingh20010801@gmail.com>
2024-03-15 09:22:11 +0530

    Time: 12 ms (83.78%), Space: 27.6 MB (13.83%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -5,17 +5,15 @@ public:
             vector<int>right(nums.size());
             vector<int>ans(nums.size());
             
-            left[0] = 1;
+            left[0]=1;
             for(int i=1; i<nums.size(); i++){
                     left[i] = left[i-1]*nums[i-1];
             }
-            
-            right[nums.size()-1] = 1;
+            right[nums.size()-1]=1;
             for(int i=nums.size()-2; i>=0; i--){
                     right[i] = right[i+1]*nums[i+1];
             }
-            
-            for(int i=0; i<ans.size(); i++){
+            for(int i=0; i<nums.size(); i++){
                     ans[i] = right[i]*left[i];
             }
             return ans;

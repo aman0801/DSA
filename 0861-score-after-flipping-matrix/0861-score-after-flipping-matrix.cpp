b445ce5e0aa5ad1a4fb47c11a8ccff3a0e662a94
Aman Singh <amansingh20010801@gmail.com>
2024-05-13 12:49:51 +0530

    Time: 0 ms (100.00%), Space: 10.2 MB (75.07%) - LeetHub

---
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int matrixScore(vector<vector<int>>& grid) {
+            int m = grid.size();
+            int n = grid[0].size();
+            
+            for(int i=0; i<m; i++){
+                    if(grid[i][0] == 0){
+                            for(int j=0; j<n; j++){
+                                    grid[i][j] = 1 - grid[i][j];
+                            }
+                    }
+            }
+            
+            for(int j=1; j<n; j++){
+                    int countZero = 0;
+                    for(int i=0; i<m; i++){
+                            if(grid[i][j] == 0){
+                                    countZero++;
+                            }
+                    }
+                    int countOne = m - countZero;
+                    if(countZero > countOne){
+                            for(int i=0; i<m; i++){
+                                  grid[i][j] = 1 - grid[i][j];  
+                            }
+                    }
+            }
+            
+            int ans = 0;
+            for(int i=0; i<m; i++){
+                    for(int j=0; j<n; j++){
+                            int tmp = grid[i][j] * pow(2, n-j-1);
+                            ans = ans + tmp;
+                    }
+            }
+            return ans;
+    }
+};

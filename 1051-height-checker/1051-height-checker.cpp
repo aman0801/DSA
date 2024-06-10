a6e2cf4b546f09e1cfb41b15074649ecd561a980
Aman Singh <amansingh20010801@gmail.com>
2024-06-10 09:19:10 +0530

    Time: 0 ms (100.00%), Space: 10 MB (96.49%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+            vector<int>tmp = heights;
+            sort(tmp.begin(), tmp.end());
+            int count=0;
+            
+            for(int i=0; i<tmp.size(); i++){
+                    if(tmp[i]!=heights[i]){
+                            count++;
+                    }
+            }
+            return count;
+    }
+};

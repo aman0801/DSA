7cfaecc4c7dab46356caebd291353cd6a23e60f0
Aman Singh <amansingh20010801@gmail.com>
2024-06-18 12:12:37 +0530

    Time: 55 ms (48.07%), Space: 41 MB (66.37%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
-            vector<pair<int, int>>arr;
-            for(int i=0; i<profit.size(); i++){
-                    arr.push_back({difficulty[i], profit[i]});
-            }
-            sort(arr.begin(), arr.end());
+        int n = difficulty.size();
+        int m = worker.size();
+        
+        vector<pair<int, int>> vec;
+        for(int i = 0; i < n; i++) {
+            vec.push_back({difficulty[i], profit[i]});
+        }
+
+        sort(begin(vec), end(vec));
+
+        sort(begin(worker), end(worker));
+
+        int totalProfit = 0;
+        int j = 0; 
+        int maxProfit = 0;
+    
+        for(int i = 0; i < m; i++) {
             
-            for(int i=1; i<arr.size(); i++){
-                    arr[i].second = max(arr[i].second, arr[i-1].second);
+            while(j < n && worker[i] >= vec[j].first) {
+                maxProfit = max(maxProfit, vec[j].second);
+                j++;
             }
-            
-            int ans=0;
-            for(int i=0; i<worker.size(); i++){
-                    int lvl = worker[i];
-                    int l = 0, r = arr.size()-1;
-                    int maxi = 0;
-                    while(l<=r){
-                            int mid = l + (r-l)/2;
-                            if(arr[mid].first<=lvl){
-                                    maxi = max(maxi, arr[mid].second);
-                                    l = mid+1;
-                            }else{
-                                    r = mid-1;
-                            }
-                    }
-                    ans = ans + maxi;
-            }
-            return ans;
+
+            totalProfit += maxProfit;
+
+        }
+
+        return totalProfit;
     }
 };
